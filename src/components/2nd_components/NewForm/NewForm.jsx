cf1932f2f8acd3a8b216e7be2b6ff554e9e2ea56
magnifiq <xbindiuk@stuba.sk>
2024-02-29 12:23:46 +0100

    add dynamic fields

--- src/components/2nd_components/NewForm/NewForm.jsx
+++ src/components/2nd_components/NewForm/NewForm.jsx
@@ -1,13 +1,25 @@
 import Button from "@mui/material/Button";
 import { TextField, Container, Grid } from "@mui/material";
-import { useForm } from "react-hook-form";
-import styles from "./NewForm.module.css"
+import { useFieldArray, useForm } from "react-hook-form";
+import styles from "./NewForm.module.css";
 export default function NewForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+    control
+  } = useForm({
+      defaultValues:{
+        additionalInfo: [{skills: '', hobbies: ''}]
+      }
+  });
+
+  const { fields, append, remove } = useFieldArray(
+    {
+      name: "additionalInfo",
+      control,
+    }
+  );
   return (
     <Container maxWidth="sm" style={{ marginTop: "20px" }}>
       <form onSubmit={handleSubmit((data) => console.log(data))}>
@@ -42,7 +54,40 @@ export default function NewForm() {
             type="password"
           />
           <p className={styles.error__input}>{errors.password?.message}</p>
+          <h3>Additional fields (optional)</h3>
+          {fields.map((field, index) => {
+            return (
+              <Grid container spacing={2} alignItems="flex-end" key={field.id} >
+                <Grid item xs={12}>
+                  <TextField
+                    sx={{ marginBottom: "10px" }}
+                    {...register(`additionalInfo.${index}.skills`)}
+                    fullWidth
+                    label="Skills"
+                    className={styles.formInput}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    sx={{ marginBottom: "10px" }}
+                    {...register(`additionalInfo.${index}.hobbies`)}
+                    fullWidth
+                    label="Hobbies"
+                    className={styles.formInput}
+                  />
+                </Grid>
+                <Grid item xs={12} className={styles.btn__remove__grid}>
+                  <Button variant="contained" onClick={() => remove(index)} >
+                    Remove
+                  </Button>
+                </Grid>
+              </Grid>
+            );
+          })}
         </Grid>
+         <Button variant="contained" onClick={() => append()} className={styles.btn__append__grid}>
+          Append
+        </Button>
         <Button type="submit" variant="contained">
           Submit
         </Button>
