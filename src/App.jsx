ce28f3c19de1253dd8282d1886c5533f39e8e1d1
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-04-11 21:33:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Form } from "./components/Form/Form";
+
 import { Table } from "./components/Table/Table";
 import DEFAULT_INPUT_VALUES from "./components/Form/defaultInputValues.js";
 import useProductData from "./hooks/useProductData";
